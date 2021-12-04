7d260d7f87be5ca40feea98f89475d7570a890a3
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2021-12-03 20:58:36 -0500

    change the timeblock color

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,27 @@
 var date = moment().format('dddd, MMMM Do');
-$('#currentDay').text(date);
+    $('#currentDay').text(date);
+
+function timechange(){
+    var hour = moment().hour();
+        console.log(hour);
+
+    $(".time-block").each(function() {
+        var blocktime = parseInt($(this).attr('id'));
+                console.log(blocktime);
+
+            if(blocktime > hour){
+                $(this).addClass('past');
+            }
+            else if (blocktime === hour){
+                $(this).addClass('present');
+            }
+            else{
+                $(this).addClass('future');
+            }
+
+    })
+};
+timechange();
+
+
 
