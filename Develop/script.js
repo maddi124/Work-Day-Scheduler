a5c4a776cc62b1e071f026214a82cf404338a579
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2021-12-04 13:50:18 -0500

    added local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,17 @@
 var date = moment().format('dddd, MMMM Do');
     $('#currentDay').text(date);
 
+ $(document).ready(function(){
+
+$('.saveBtn').on('click',function(){
+    console.log('it was clicked');
+    var text = $(this).siblings('#description').val();
+    var time = $(this).parent().attr('id');
+
+    localStorage.setItem(JSON.stringify(time),JSON.stringify(text));
+
+});
+
 function timechange(){
     var hour = moment().hour();
         console.log(hour);
@@ -21,7 +32,27 @@ function timechange(){
 
     })
 };
+
 timechange();
+});
+
+$('#9am .description').val(localStorage.getItem('9am'));
+$('#10am .description').val(localStorage.getItem('10am'));
+$('#11am .description').val(localStorage.getItem('11am'));
+$('#12pm .description').val(localStorage.getItem('12pm'));
+$('#13pm .description').val(localStorage.getItem('13pm'));
+$('#14pm .description').val(localStorage.getItem('14pm'));
+$('#15pm .description').val(localStorage.getItem('15pm'));
+$('#16pm .description').val(localStorage.getItem('16pm'));
+$('#17pm .description').val(localStorage.getItem('17pm'));
+$('#18pm .description').val(localStorage.getItem('18pm'));
+$('#19pm .description').val(localStorage.getItem('19pm'));
+$('#20pm .description').val(localStorage.getItem('20pm'));
+$('#21pm .description').val(localStorage.getItem('21pm'));
 
 
 
+
+
+
+// localStorage.clear();
